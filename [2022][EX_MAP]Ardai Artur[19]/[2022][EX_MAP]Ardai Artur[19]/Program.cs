4da16facb6993aaaf15ccf5cc72cee77ddca0fcb
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2022-02-02 22:52:21 -1200

    Add files via upload

---
+++ [2022][EX_MAP]Ardai Artur[19]/[2022][EX_MAP]Ardai Artur[19]/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+namespace _2022__EX_MAP_Ardai_Artur_19_
+{
+    class Program
+    {
+        //19
+        public static async Task unu(int[] a, int k, int lim)
+        {
+            int db = 0;
+            using TextWriter afara = new StreamWriter(@"..\..\combinari.out");
+            if(db<=lim)
+            for (int i = 1; i <= k; ++i)
+            { await afara.WriteAsync(Convert.ToString(a[i]+" "));
+                db++;
+            }
+        }
+       public static  void comb(int k,int[]v,int n,int lim)
+        {
+            
+
+
+            for (int i = v[k - 1] + 1; i <= n; ++i)
+            {
+                v[k] = i;
+                unu(v, k,lim);
+                comb(k + 1,v,n,lim);
+            }
+        }
+     
+        static void Main(string[] args)
+        {
+            TextReader load = new StreamReader(@"..\..\combinari.in");
+            string buffer = load.ReadLine();
+            int n = int.Parse(buffer.Split(' ')[0]);
+            int k = int.Parse(buffer.Split(' ')[1]);
+            int[] v = new int[100];
+
+            comb(1, v, n,k);
+
+            
+
+
+        }
+    }
+}
