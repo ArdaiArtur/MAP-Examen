db62039b2fc3d455c87b0d97aaba2212dd2980c9
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2022-02-02 22:51:00 -1200

    Add files via upload

---
+++ [2022][EX_MAP]Ardai Artur[10]/[2022][EX_MAP]Ardai Artur[10]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2022__EX_MAP_Ardai_Artur_10_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //10
+            string s = Console.ReadLine();
+            int n = int.Parse(s.Split(' ')[0]);
+            int m = int.Parse(s.Split(' ')[1]);
+            int a = 0;
+            if (m * 2 >= n) Console.Write(0 + " ");
+            else Console.Write(n - 2 * m + " ");
+            while (a * (a - 1) / 2 < m) a++;
+
+            Console.Write(n-a);
+           
+        }
+    }
+}
